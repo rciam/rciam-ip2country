1648e0f4dc520639f444a3b4fa596984954f72e2
Nick Mastoris <nmastoris@cti.gr>
2021-01-26 15:26:13 +0200

    add logging messages, put tables' name configuration

--- ipToCountry.py
+++ ipToCountry.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python
 from Controller.countryStatisticsController import countryStatisticsController
 from Model.countryStatistics import countryStatistics
-from Model.geoipDatabase import geoipDatabase
+from Service.ipDatabase import geoipDatabase
 import ipaddress
+import log
 
 class ipToCountry:
+  logger = log.get_logger("ipToCountry")
+
   @classmethod
   def mapIpToCountry(self):
     # handler for ip databases
-    databaseHandler = geoipDatabase()
+    ipDatabaseHandler = geoipDatabase()
     ipData = countryStatisticsController.getDataNotMapped()
+    savedItems = 0 
     for item in ipData:
       # get network address
       ipaddr = ipaddress.ip_network(item.ip).network_address
       # print(ipaddr)
       # get country code
-      countryCode = databaseHandler.getCountryFromIp(str(ipaddr), item.ipVersion)
+      countryCode = ipDatabaseHandler.getCountryFromIp(str(ipaddr), item.ipVersion)
       if(countryCode != None):
         countryStatisticsItem = countryStatistics(None, item.accessed, item.sourceIdp, item.service, countryCode, 1)
         countryStatistics.save(countryStatisticsItem)
-
+        savedItems +=1
+      else:
+        self.logger.warning("ip {0} not found at database".format(ipaddr))
+    self.logger.info("{0} ips mapped to countries".format(savedItems))
 
       
 
 
-ipToCountry.mapIpToCountry()
+#ipToCountry.mapIpToCountry()
     
