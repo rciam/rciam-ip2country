6e14cbc07b2d6a67ba7c8163f2adc9be7db8df55
Nick Mastoris <nmastoris@cti.gr>
2021-03-08 21:17:27 +0200

    change folder location of databases

--- Service/ipDatabase.py
+++ Service/ipDatabase.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from Model.ipStatistics import ipStatistics
+from Utils import configParser
 import GeoIP
 
 class ipDatabase(ABC):
@@ -10,8 +11,8 @@ class ipDatabase(ABC):
 class geoipDatabase(ipDatabase):
   @classmethod
   def getCountryFromIp(self, ip, ipVersion):
-    gi = GeoIP.open("../databases/GeoIP.dat",GeoIP.GEOIP_STANDARD)
-    giV6 =  GeoIP.open("../databases/GeoIPv6.dat",GeoIP.GEOIP_STANDARD)
+    gi = GeoIP.open("./databases/GeoIP.dat",GeoIP.GEOIP_STANDARD)
+    giV6 =  GeoIP.open("./databases/GeoIPv6.dat",GeoIP.GEOIP_STANDARD)
     if ipVersion == 'ipv4':     
       return [gi.country_code_by_addr(ip),gi.country_name_by_addr(ip)]
     else:     
